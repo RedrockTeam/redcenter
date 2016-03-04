04a75c91b980e15a02e957abf1488b5a9ffb0d63
zxy <304246725@qq.com>
2016-03-04 09:37:48 +0800

    common

---
+++ member/src/js/common.js
@@ -0,0 +1,26 @@
+/**
+ * Created by zxy on 2016/3/3.
+ */
+//;(function($){
+    var $selectImg = $('.content-li-img'),
+        imgLen = $selectImg.length,
+        $select = $('.content-left-li');
+    for(var i = 0;i < imgLen;i++){
+        $($selectImg[i]).css('background-position',(240-24*i)+'px'+' 0');
+    }
+    $select.on('click',function(e){
+        var $checked = $('.content-li-clicked'),
+            $oldImg = $checked.find('.content-li-img'),
+            $img = $(this).find('.content-li-img');
+        if($checked.length){
+            $oldImg.css('background-position',parseInt($oldImg.css('background-position'))+120+'px'+' 0');
+            $checked.find('.content-li-bar').css('width','0px');
+            $checked.removeClass('content-li-clicked');
+        }
+        $(this).addClass('content-li-clicked');
+        $img.css('background-position',parseInt($img.css('background-position'))-120+'px'+' 0');
+        $(this).find('.content-li-bar').css('width','4px');
+        //console.log(parseInt($img.css('background-position')));
+    });
+    //alert(1);
+//}(jQuery));
