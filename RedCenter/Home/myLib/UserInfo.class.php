25ec1aa63f44516c75c25c96d6c4e63152daeff7
Mr_liang <578423625@qq.com>
2016-03-05 19:22:39 +0800

    d

--- RedCenter/Home/myLib/UserInfo.class.php
+++ RedCenter/Home/myLib/UserInfo.class.php
@@ -52,6 +52,7 @@ class UserInfo {
             $all_scores['byMonth']["$key"] = 0;
             foreach ($logs as $once) {
                 $all_scores['byMonth']["$key"] += $once['score'];
+                if($all_scores['byMonth']["$key"] <0 )    $all_scores['byMonth']["$key"] = 0;
             }
             $all_scores['byMonth']['total'] += $all_scores['byMonth']["$key"]; 
         }
@@ -63,8 +64,10 @@ class UserInfo {
             $all_scores['byYear']["$key"] = 0;
             foreach ($logs as $once) {
                 $all_scores['byYear']["$key"] += $once['score'];
+                if($all_scores['byYear']["$key"] <0 )     $all_scores['byYear']["$key"] = 0;
             }
-            $all_scores['byYear']['total'] += $all_scores['byYear']["$key"]; 
+            $all_scores['byYear']['total'] += $all_scores['byYear']["$key"];
+
         }
         
          return $all_scores;
@@ -132,8 +135,8 @@ class UserInfo {
         $last_year_rank = $this->info['last_year_rank'];
         $change['rankchange_month'] = $this->info['last_month_rank']==0 ? 0 : $this->info['last_month_rank'] - $this->info['month_rank']; 
         $change['rankchange_year'] = $this->info['last_year_rank']==0 ? 0 : $this->info['last_year_rank'] - $this->info['year_rank'];
-        $change['month_src'] = $change['rankchange_month'] > 0 ? 'rose.png' : 'decline.png';
-        $change['year_src'] = $change['rankchange_year'] > 0 ? 'rose.png' : 'decline.png';        
+        $change['month_src'] = $change['rankchange_month'] >= 0 ? 'rose.png' : 'decline.png';
+        $change['year_src'] = $change['rankchange_year'] >= 0 ? 'rose.png' : 'decline.png';
         return $change;
     }
 
