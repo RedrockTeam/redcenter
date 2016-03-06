2b8fa2c53e0650e28178aac03aef93beff8c2fbb
GiantMing <971334483@qq.com>
2016-03-06 10:56:33 +0800

    咱不动态写fixed了, footerpadding加上

--- member/src/js/common.js
+++ member/src/js/common.js
@@ -29,35 +29,43 @@
     var $selectImg = $('.content-li-img'),
         imgLen = $selectImg.length,
         $select = $('.content-left-li');
-    for(var i = 0;i < imgLen; i++){
+    for(var i = 0; i < imgLen; i++){
         $($selectImg[i]).css('background-position-x', 24 * i + 'px');
     }
-    $select.on('click',function(e){
+    $select.on('click', function(e){
         var $checked = $('.content-li-clicked'),
             $oldImg = $checked.find('.content-li-img'),
             $img = $(this).find('.content-li-img');
+
         if($checked.length){
             $oldImg.css('background-position-y', 0);
-            $checked.find('.content-li-bar').css('width','0px');
+            $checked.find('.content-li-bar').css('width', 0);
             $checked.removeClass('content-li-clicked');
         }
+        
         $(this).addClass('content-li-clicked');
         $img.css('background-position-y', 24 + 'px');
         $(this).find('.content-li-bar').css('width','4px');
     });
 
 
-
-    window.onload = window.onresize = function() {
+    // 对侧边栏定位
+/*    function fixedSideBar() {
         var docWidth = document.documentElement.offsetWidth;
+
         setFixedLeft($('.content-left'));
         setFixedLeft($('.header-left'));
+
         function setFixedLeft(obj) {
             obj.css('left', docWidth/2-496);
             if(docWidth < 1080) {
                 obj.css('left', 0);
             }
         }
+
     }
 
+    window.addEventListener('load', fixedSideBar);
+    window.addEventListener('resize', fixedSideBar);*/
+    
 }(jQuery));
