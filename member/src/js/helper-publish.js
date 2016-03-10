8a8971ad0a46c09664ca0c83fe881816485827d1
GiantMing <971334483@qq.com>
2016-03-10 21:31:04 +0800

    加了个ueditor 视觉姐姐不要砍我

--- member/src/js/helper-publish.js
+++ member/src/js/helper-publish.js
@@ -1,10 +1,39 @@
 var doc = document;
-var select = doc.querySelector('#select-product');
+var select = doc.querySelectorAll(".select");
+var select_product = doc.querySelector('#select-product');
 var sec_select = doc.querySelector('#sec-product-wrap');
-var choose_select = doc.querySelector('#choosed-product');
+var choose_select = doc.querySelector('#dot-border-choosed');
 
 
-select.addEventListener('click', function(event) {
+function addClassName(ele, className) {
+	var preClass = ' ' +  ele.className;
+	var reg = new RegExp(' ' + className);
+	if(reg.test(' ' + preClass)) {
+		return;
+	}
+	ele.className += ' ' + className;
+}
+
+
+function removeClassName(ele, className) {
+	var preClass = ' ' +  ele.className;
+	ele.className = preClass.replace(' ' + className, '').trim();
+
+}
+
+
+
+select[0].addEventListener('click', function() {
+	addClassName(select[0].children[0], 'dot-border-choosed');
+	removeClassName(select[1].children[0], 'dot-border-choosed');
+});
+
+select[1].addEventListener('click', function() {
+	addClassName(select[1].children[0], 'dot-border-choosed');
+	removeClassName(select[0].children[0], 'dot-border-choosed');
+});
+
+select_product.addEventListener('click', function(event) {
 	var event = event || window.event;
 	var target = event.target || event.srcElement;
 
@@ -17,11 +46,5 @@ select.addEventListener('click', function(event) {
 		choose_select.innerHTML = target.children[0].innerHTML;
 	}
 
-	// console.log(target);
 });
 
-
-// select.addEventListener('mouseover', function() {
-// 	var	sec_style = sec_select.style;
-// 	sec_style.display = 'none';
-// })
