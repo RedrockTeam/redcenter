6628a41275cbcdd9b3820cf4e84997edb7f03ac8
GiantMing <971334483@qq.com>
2016-03-12 21:59:27 +0800

    fixed helper-publish product select bug

--- member/public/js/helper-publish.js
+++ member/public/js/helper-publish.js
@@ -1,32 +1,34 @@
-var doc = document;
-var select = doc.querySelectorAll(".select");
-var select_product = doc.querySelector('#select-product');
-var sec_select = doc.querySelector('#sec-product-wrap');
-var choose_select = doc.querySelector('#dot-border-choosed');
+;(function(doc){
+	var select = doc.querySelectorAll(".select"),
+		select_product = doc.querySelector('#select-product'),
+		sec_select = doc.querySelector('#sec-product-wrap'),
+		choose_select = doc.querySelector('#choosed-product');
 
 
-select[0].addEventListener('click', function() {
-	addClassName(select[0].children[0], 'dot-border-choosed');
-	removeClassName(select[1].children[0], 'dot-border-choosed');
-});
 
-select[1].addEventListener('click', function() {
-	addClassName(select[1].children[0], 'dot-border-choosed');
-	removeClassName(select[0].children[0], 'dot-border-choosed');
-});
+	addEven(select[0], 'click', function() {
+		addClass(select[0].children[0], 'dot-border-choosed');
+		removeClass(select[1].children[0], 'dot-border-choosed');
+	});
+	addEven(select[1], 'click', function() {
+		addClass(select[1].children[0], 'dot-border-choosed');
+		removeClass(select[0].children[0], 'dot-border-choosed');
+	});
 
-select_product.addEventListener('click', function(event) {
-	var event = event || window.event;
-	var target = event.target || event.srcElement;
+	
+	addEven(select_product, 'click', function(event) {
+		var event = event || window.event;
+		var target = event.target || event.srcElement;
 
-	var	sec_style = sec_select.style;
-	sec_style.display = sec_style.display === 'none'? 'block': 'none';
+		var	sec_style = sec_select.style;
+		sec_style.display = sec_style.display === 'none'? 'block': 'none';
 
-	if(target.tagName.toLowerCase() === 'a') {
-		choose_select.innerHTML = target.innerText;
-	} else if(target.tagName.toLowerCase() === 'li') {
-		choose_select.innerHTML = target.children[0].innerHTML;
-	}
+		if(target.tagName.toLowerCase() === 'a') {
+			choose_select.innerHTML = target.innerText;
+		} else if(target.tagName.toLowerCase() === 'li') {
+			choose_select.innerHTML = target.children[0].innerHTML;
+		}
+	});
+}(document));
 
-});
 
