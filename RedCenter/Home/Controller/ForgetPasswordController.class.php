3feaa204b0bbeb99e4b4681e5df04a41421a68d9
Lich2013 <longzy2013@gmail.com>
2016-05-05 15:44:15 +0800

    init forgetpassword

---
+++ RedCenter/Home/Controller/ForgetPasswordController.class.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Lich
+ * Date: 16/5/5
+ * Time: 15:40
+ */
+namespace Home\Controller;
+use Org\Util\String;
+use Think\Controller;
+class ForgetPasswordController extends Controller {
+
+}
