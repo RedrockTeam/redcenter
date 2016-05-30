97d441ed204e537af070e641d381ab2f0ad10134
Mr_liang <578423625@qq.com>
2016-05-30 20:59:49 +0800

    微信测试

--- RedCenter/Home/myLib/UserInfo.class.php
+++ RedCenter/Home/myLib/UserInfo.class.php
@@ -117,7 +117,7 @@ class UserInfo {
 //            $all_scores['byMonth']['total'] += $all_scores['byMonth']["$key"];
 //        }   //array('user_id' => $this->info['id'], 'project' => "$value",'create_time'=>array('BETWEEN',"$month_start,$month_end") )
         foreach($projects as $key => $value) {
-            $one_score = M('user_log')->where(array('user_id' => $this->info['id'], 'project' => "$value",'create_time'=>array('BETWEEN',"$month_start,$month_end") ))->sum('score');
+            $one_score = M('user_log')->sum('score');
             $all_scores['byMonth']["$key"]  = $one_score>0 ? $one_score : 0;
             $all_scores['byMonth']['total'] += $all_scores['byMonth']["$key"];
         }
@@ -138,7 +138,7 @@ class UserInfo {
 //        }
         //array('user_id' => $this->info['id'], 'project' => "$value", 'create_time'=>array('BETWEEN',"$year_start,$year_end"))
         foreach($projects as $key => $value) {
-            $one_score = M('user_log')->where(array('user_id' => $this->info['id'], 'project' => "$value", 'create_time'=>array('BETWEEN',"$year_start,$year_end")))->sum('score');
+            $one_score = M('user_log')->sum('score');
             $all_scores['byYear']["$key"]  = $one_score>0 ? $one_score : 0;
             $all_scores['byYear']['total'] += $all_scores['byYear']["$key"];
         }
