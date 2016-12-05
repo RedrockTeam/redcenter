96e445fdbd43e945525f2fa8a2c3e57284020bb0
Mr_liang <578423625@qq.com>
2016-12-05 18:56:46 +0800

    a

--- RedCenter/Home/Conf/config.php
+++ RedCenter/Home/Conf/config.php
@@ -10,5 +10,5 @@ return array(
     'DB_NAME'   => 'MagicLoop',//'DB_NAME'   => 'MagicLoop',    // 数据库名
     'DB_USER'   => 'redrock',//'DB_USER'   => 'redrock',        // 用户名
     'DB_PWD'    => 'hongyanredrock',//'DB_PWD'    => 'hongyanredrock',  // 密码
-    'DB_PORT'   => '3306', // 端口
+    'DB_PORT'   => '3306',                                      // 端口
 );
